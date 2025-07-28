64f6c1f81df4ae02048b85170bef7345e6b8b66f
chanlinna <chanlinnalin@gmail.com>
2025-07-28 10:58:02 +0700

    home page

--- Customer-Frontend/src/components/TestimonialsSection/TestimonialsSection.jsx
+++ Customer-Frontend/src/components/TestimonialsSection/TestimonialsSection.jsx
@@ -5,15 +5,15 @@ import TestimonialCard from '../TestimonialCard/TestimonialCard'; // Path to Tes
 function TestimonialsSection() {
     const testimonials = [
         {
-            quote: "EventEase made our wedding day absolutely perfect. Everything was handled with such care and professionalism.",
+            quote: "EventNA made our wedding day absolutely perfect. Everything was handled with such care and professionalism.",
             author: "- Emily R."
         },
         {
-            quote: "Our corporate retreat was a huge success thanks to the meticulous planning by EventEase.",
+            quote: "Our corporate retreat was a huge success thanks to the meticulous planning by EventNA.",
             author: "- Mark T."
         },
         {
-            quote: "From start to finish, EventEase was there to make our launch event a memorable experience.",
+            quote: "From start to finish, EventNA was there to make our launch event a memorable experience.",
             author: "- Sarah L."
         }
     ];
--- Customer-Frontend/src/components/VenueCard/VenueCard.jsx
+++ Customer-Frontend/src/components/VenueCard/VenueCard.jsx
@@ -1,6 +1,7 @@
 // VenueCard.js
 import React from 'react';
 import { Link } from 'react-router-dom'; 
+import { FaMapMarkerAlt } from 'react-icons/fa';
 import './VenueCard.css'; 
 
 const VenueCard = ({ venue }) => {
@@ -23,16 +24,18 @@ const VenueCard = ({ venue }) => {
           <h3 className="venue-name">{venue.name}</h3>
 
           {/* Venue Location */}
-          <p className="venue-location">{venue.location}</p>
+          <p className="venue-location">
+            <FaMapMarkerAlt className="location-icon" /> {venue.location}
+          </p>
 
           <div className="venue-specs">
             {/* Max Occupancy */}
-            <span className="max-occupancy">Max Capacity: {venue.max_occupancy} guests</span>
+            <span className="max-occupancy">Max member: {venue.max_occupancy}</span>
             {/* Price */}
             <span className="price">${venue.price} per day</span>
           </div>
 
-          <button className="book-button">Book this Venue</button>
+          <button className="book-button" style={{background : "#5D8191"}}>Book this Venue</button>
         </div>
       </div>
     </Link>
